0a3aa45d7e7ccb4a419c052a58bd380cc56c8441
Han Koh <rhgksrua@gmail.com>
2016-08-20 00:19:55 -0500

    fixed css

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,24 @@
+// Close when X clicked
 $('.close').click(function(e) {
     $('.contact-container').removeClass('show');
     e.preventDefault();
     console.log('closing contact');
 });
 
+// Close when clicked outside form
+$('.contact-container').click(function(e) {
+    if (e.target !== e.currentTarget) return;
+    $(this).removeClass('show');
+});
+
+// Open contact
 $('#contact').click(function(e) {
     e.preventDefault();
     $('.contact-container').addClass('show');
     console.log('open contact');
+});
+
+$('#submit-msg').click(function(e) {
+    console.log('submit');
+    e.preventDefault();
 });
